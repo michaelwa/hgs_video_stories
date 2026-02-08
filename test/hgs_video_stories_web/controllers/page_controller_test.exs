99e9c6231c55b4202d1f4147cabd2d917c4da102
Michael A. Johnson <michael@intandemhealth.net>
2026-02-08 08:53:47 -0800

    Replace home boilerplate with themed landing stub

--- test/hgs_video_stories_web/controllers/page_controller_test.exs
+++ test/hgs_video_stories_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule HgsVideoStoriesWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Record short video stories directly from your browser."
   end
 end
